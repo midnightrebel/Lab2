9acded9aca9cc31a50e7cc4bfbf75da328274370
midnightrebel <47571243+midnightrebel@users.noreply.github.com>
2020-04-02 19:45:00 +0400

    Update Die.cs

--- task2/Assets/Scripts/Die.cs
+++ task2/Assets/Scripts/Die.cs
@@ -1,30 +1,79 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Die : MonoBehaviour
 {
-
+    public delegate void DeadEvent();
+    public event DeadEvent onDeath;
+    
+    public Text countDeath;
+    public int dead = 0;
     public GameObject respawn;
-    private GameObject Player;
-
-
-    public UnityEvent DeadEvent;
-
-    void OnTriggerEnter2D (Collider2D other)
+    public GameObject Player;
+    public bool hardMode;
+    void Start()
+    {
+        onDeath += EasyMode;
+    }
+    public void Respawn()
+    {
+        Player.transform.position = respawn.transform.position;
+    }
+    public void SwitchMode()
+    {
+        if(hardMode == false)
+        {
+            hardMode = true;
+            onDeath += HardMode;
+            onDeath -= EasyMode;
+        }
+        else
+        {
+            onDeath += EasyMode;
+            onDeath -= HardMode;
+            hardMode = false;
+        }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            SwitchMode();
+        }
+    }
+    public void CountDeath()
+    {
+        dead++;
+        countDeath.text = "You died " + dead.ToString() + " times";
+    }
+    public void EasyMode()
+    {
+        Respawn();
+        CountDeath();    
+    }
+    public void HardMode()
+    {
+        if(dead < 3)
+        {
+            Respawn();
+            CountDeath();
+        }
+        else
+        {
+            Destroy(Player);
+            SceneManager.LoadScene("Gameover");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Player = other.gameObject;
-            DeadEvent.Invoke();
+            onDeath();
         }
-    }
-
-    public void Respawn()
-    {
-        Player.transform.position = respawn.transform.position;
     }
 
 }
