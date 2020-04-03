0ab78f0cd4c3847f360d7aae568ab851c63c4dd5
midnightrebel <47571243+midnightrebel@users.noreply.github.com>
2020-04-03 09:33:18 +0400

    New Update Die.cs

--- task2/Assets/Scripts/Die.cs
+++ task2/Assets/Scripts/Die.cs
@@ -4,75 +4,101 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class Die : MonoBehaviour
+public class EasyClass 
 {
-    public delegate void DeadEvent();
-    public event DeadEvent onDeath;
-    
-    public Text countDeath;
-    public int dead = 0;
-    public GameObject respawn;
-    public GameObject Player;
-    public bool hardMode;
-    void Start()
-    {
-        onDeath += EasyMode;
-    }
-    public void Respawn()
-    {
-        Player.transform.position = respawn.transform.position;
-    }
-    public void SwitchMode()
-    {
-        if(hardMode == false)
-        {
-            hardMode = true;
-            onDeath += HardMode;
-            onDeath -= EasyMode;
-        }
-        else
-        {
-            onDeath += EasyMode;
-            onDeath -= HardMode;
-            hardMode = false;
-        }
-    }
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.M))
-        {
-            SwitchMode();
-        }
-    }
-    public void CountDeath()
-    {
-        dead++;
-        countDeath.text = "You died " + dead.ToString() + " times";
-    }
+    public GameObject respawnE;
+    public GameObject PlayerE;
     public void EasyMode()
     {
         Respawn();
-        CountDeath();    
     }
+    public void Respawn()
+    {
+        PlayerE.transform.position = respawnE.transform.position;
+    }
+}
+public class HardClass 
+{
+    public int dead = 0;
+    public GameObject respawnH;
+    public GameObject PlayerH;
     public void HardMode()
     {
-        if(dead < 3)
+        if (dead < 3)
         {
             Respawn();
             CountDeath();
         }
         else
         {
-            Destroy(Player);
             SceneManager.LoadScene("Gameover");
         }
     }
+    public void Respawn()
+    {
+        PlayerH.transform.position = respawnH.transform.position;
+    }
+    public void CountDeath()
+    {
+        dead++;
+    }
+
+}
+public class EventHandler
+{
+    public delegate void DeadEvent();
+    public event DeadEvent onDeath;
+    public void DieMethod()
+    {
+        onDeath();
+    }
+}
+public class Die : MonoBehaviour
+{
+    EventHandler DeathEvent = new EventHandler();
+    EasyClass easy = new EasyClass();
+    HardClass hard = new HardClass();
+    public GameObject respawn;
+    public GameObject Player;
+    public bool hardMode;
+    void Start()
+    {
+        DeathEvent.onDeath += easy.EasyMode;
+        easy.respawnE = respawn;
+        easy.PlayerE = Player;
+        hard.PlayerH = Player;
+        hard.respawnH = respawn;
+    }
+    public void SwitchMode()
+    {
+        if (hardMode == false)
+        {
+            hardMode = true;
+            DeathEvent.onDeath += hard.HardMode;
+            DeathEvent.onDeath -= easy.EasyMode;
+        }
+        else
+        {
+            DeathEvent.onDeath += easy.EasyMode;
+            DeathEvent.onDeath -= hard.HardMode;
+            hardMode = false;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SwitchMode();
+        }
+    }
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Player = other.gameObject;
-            onDeath();
+            DeathEvent.DieMethod();
         }
     }
 
