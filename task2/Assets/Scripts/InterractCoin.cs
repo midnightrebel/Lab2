6423f28fb4f4c0b35af7c2cefc2abbce27d6d8d9
midnightrebel <47571243+midnightrebel@users.noreply.github.com>
2020-04-06 15:24:24 +0400

    Уничтожаемая монетка

---
+++ task2/Assets/Scripts/InterractCoin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterractCoin :  Intaract
+{
+    public override void Use(GameObject gameObject)
+    {
+        base.Use(this.gameObject);
+        Destroy(this.gameObject);
+    }
+}
